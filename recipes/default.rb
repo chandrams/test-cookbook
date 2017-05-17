477b5da52979e9dd50aab14d2b241499faefc6a6
Chandrakala M Subramanyam <chandra-ms@in.ibm.com>
2017-05-17 14:06:58 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-file "/root/chandra/chef/hello.txt" do
+file "./hello.txt" do
 	content "Hello, this is my first cookbook recipe\n how are you?"
 action :create
 end
-arch = node['travis_java']['arch']
-puts "arch = #{arch}"
+
+execute "cat ./hello.txt"

9613797fb38d33993313a15f398e60d5670d2916
Chandrakala <chandra-ms@in.ibm.com>
2017-05-17 14:19:33 +0530

    Added recipe to install java

---
+++ libraries/ibmjava.rb
@@ -0,0 +1,69 @@
+require 'yaml'
+require 'open-uri'
+require 'fileutils'
+
+module TravisJava
+  module IBMJava
+    def install_ibmjava(version)
+      attribute_key = "ibmjava" + version.to_s
+      java_home = ::File.join(node['travis_java']['jvm_base_dir'], node['travis_java'][attribute_key]['jvm_name'])
+      index_yml = ::File.join("https://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/meta/sdk",
+                              node['travis_java']['ibmjava']['platform'], node['travis_java']['ibmjava']['arch'], "index.yml")
+
+      # Obtain the uri of the latest IBM Java build for the specified version from index.yml
+      entry = find_version_entry(index_yml, version)
+      # Download and install the IBM Java build
+      download_build(entry, java_home, version)
+    end
+
+    # This method downloads and installs the java build
+    # @param [Hash] entry - latest entry from the index.yml for the specified ibm java version containing uri
+    # @param [String] java_home - directory path where IBM Java will be installed
+    # @param [String] version - java version
+    # @return - None
+
+    def download_build(entry, java_home, version)
+      installer = File.join(Dir.tmpdir, "ibm-java" + version.to_s + "-installer")
+      properties = File.join(Dir.tmpdir, "installer.properties")
+
+      # Download the IBM Java installer from source url to the local machine
+      remote_file installer do
+        src_url = entry['uri']
+        source src_url.to_s
+        mode '0755'
+        checksum entry['sha256sum']
+        action :create
+        not_if "test -f #{installer}"
+      end
+
+      # Create installer properties for silent installation
+      file properties do
+        content "USER_INSTALL_DIR=#{java_home}\nLICENSE_ACCEPTED=TRUE\n"
+      end
+
+      # Install IBM Java build
+      execute "#{installer} -i silent"
+      execute "#{java_home}/bin/java -version"
+
+      file properties do
+        action :delete
+      end
+    end
+
+    # This method returns a hash containing the uri and checksum of the latest release by parsing the index.yml file
+    # @param [String] url - url of index.yml file
+    # @param [String] version - java version
+    # @return [Hash] finalversion - latest entry from the index.yml for the specified IBM Java version containing uri
+    #                               and sha256sum
+    def find_version_entry(url, version)
+      finalversion = nil
+      version = '1.'.concat(version.to_s) unless version.to_s.include?('.')
+      yaml_content = open(url.to_s, &:read)
+      thing = YAML.safe_load(yaml_content)
+      thing.each do |thi|
+        finalversion = thi[1] if thi[0].to_s.start_with?(version.to_s)
+      end
+      finalversion
+    end
+  end
+end

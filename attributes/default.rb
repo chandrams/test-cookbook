c7d79c91faf2756915c0743e241682ca659e96e5
Chandrakala M Subramanyam <chandra-ms@in.ibm.com>
2017-06-20 15:40:44 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,9 @@
 default['travis_java']['jdk_switcher_url'] = 'https://raw.githubusercontent.com/michaelklishin/jdk_switcher/565b95b8946abf8ce3f2b0cc87fb8260a3d5aa3c/jdk_switcher.sh'
 default['travis_java']['jdk_switcher_path'] = '/opt/jdk_switcher/jdk_switcher.sh'
 default['travis_java']['jvm_base_dir'] = '/usr/lib/jvm'
+default['travis_java']['arch'] = 'i386'
+default['travis_java']['arch'] = 'amd64' if node['kernel']['machine'] =~ /x86_64/
 
 default['travis_java']['ibmjava']['platform'] = 'linux'
-default['travis_java']['ibmjava']['arch'] = 'x86_64'
-default['travis_java']['ibmjava8']['jvm_name'] = 'java-8-ibm'
-default['travis_java']['ibmjava9']['jvm_name'] = 'java-9-ibm'
+default['travis_java']['ibmjava8']['jvm_name'] = "java-8-ibm-#{node['travis_java']['arch']}"
+default['travis_java']['ibmjava9']['jvm_name'] = "java-9-ibm-#{node['travis_java']['arch']}"
